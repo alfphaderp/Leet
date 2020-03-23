a2b8b9fc381bca79602d65f9e106392dfe23a92f
Ryan Zhu <36178918+alfphaderp@users.noreply.github.com>
2020-03-22 21:21:28 -0700

    Remove permutation print

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,11 @@ import leetspeak.LeetEncoder;
 
 public class Main {
 	public static void main(String[] args) {
-		LeetEncoder le = new LeetEncoder("./res/ultimate.leet");
+		LeetEncoder le = new LeetEncoder("./res/regionalemoji.leet");
 		try(Scanner in = new Scanner(System.in)) {
 			while(true) {
 				String input = in.nextLine();
-				System.out.println(le.encode(input) + " (" + le.getPermutations(input) + ")");
+				System.out.println(le.encode(input));
 			}
 		}
 	}
